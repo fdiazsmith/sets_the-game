539360e363cc0fe2d7bc5c82f844786947c0b308
Fernando Diaz Smith <fdiazsmith@gmail.com>
2015-06-19 16:27:54 -0400

    find all posible permutations

--- sets_the-game.js
+++ sets_the-game.js
@@ -12,7 +12,7 @@ if (Meteor.isClient) {
  var randomPropertyAssigment = function(){
     return Math.min( Math.max(Math.floor(Math.random()*10/3) , 0), 2);
   };
-  
+
 
   /*
     * Global Variables
@@ -21,11 +21,15 @@ if (Meteor.isClient) {
   card = Template.card;
   grid = Template.sets_grid;
 
-  colors  = ["orange", "green"  , "blue"    ]; 
+  colors  = ["orange", "green"  , "blue"    ];
   shapes  = ["x"     , "circle" , "diamond" ];
   fills   = ["solid" ,  "hashed", "hollow"  ];
-
-  features = ["color", "shape", "fill"];
+  allPossibleCards = [];
+  numberOfCards = 0;
+  firstPick = [1, 2, 3, 4,  5,  6,  7,  8,  9,  10,  11,  12];
+  secondPick =['1d', '2d', '3d', '4d',  '5d',  '6d',  '7d',  '8d',  '9d',  '10d',  '11d',  '12d'];
+  thridPick = ['1q', '2q', '3q', '4q',  '5q',  '6q',  '7q',  '8q',  '9q',  '10q',  '11q',  '12q'];
+       features = ["color", "shape", "fill"];
 
   // Game.insert({ "color": colors[randomPropertyAssigment],
   //               "shape": shapes[randomPropertyAssigment],
@@ -55,7 +59,92 @@ if (Meteor.isClient) {
       console.log("removing  Card"    );
       Game.remove(  Game.find({}, {sort: {createdAt: -1}}).fetch()[0]._id );
     },
+    "click #pattern-check": function () {
+      var currentGame = Game.find({});
+      var arrayOne = new Array();
+      count = 0;
+      allPosiblePermutation([firstPick,firstPick,firstPick] );
+      // allPosibleCombinations(firstPick, 3);
+      currentGame.forEach(function (card){
+        console.log(card);
+        // number = 0;
+        // arrayOne[count] = (card.color === "orange" )? 3 : (card.color === "blue")?  6 : 9;
+        // count++;
+
+      });
+
+
+      console.log("Game", allPossibleCards, arrayOne);
+
+    }
   });
+  /*NOTE: The number of combinations of size r drawn from an input of size n is
+          referred to as nCr, which is equal to n! ÷ [r!(n-r)!].
+  */
+  function multiply(a, b) {
+    var aNumRows = a.length, aNumCols = a[0].length,
+        bNumRows = b.length, bNumCols = b[0].length,
+        m = new Array(aNumRows);  // initialize array of rows
+    for (var r = 0; r < aNumRows; ++r) {
+      m[r] = new Array(bNumCols); // initialize the current row
+      for (var c = 0; c < bNumCols; ++c) {
+        m[r][c] = 0;             // initialize the current cell
+        for (var i = 0; i < aNumCols; ++i) {
+          m[r][c] += a[r][i] * b[i][c];
+        }
+      }
+    }
+    return m;
+  }
+  function allPosiblePermutation(choices, prefix) {
+    numberOfCards++;
+    // console.log(choices,choices.slice(1),(prefix || []).concat(choices[0][c]))
+    if(!choices.length) {
+      allPossibleCards[numberOfCards] = prefix;
+        return console.log(prefix); ;
+    }
+    for(var c = 0; c < choices[0].length; c++) {
+      // debugger;
+      // console.log(choices,choices.slice(1),(prefix || []).concat(choices[0][c]))
+      try {
+
+        allPosiblePermutation(choices.slice(1), (prefix || []).concat(choices[0][c]));
+      } catch (e) {
+        // console.log("Error is here: ",e);
+      } finally {
+
+      }
+
+      // console.log(choices,choices.slice(1),(prefix || []).concat(choices[0][c]))
+    }
+  }
+
+  function allPosibleCombinations(base, numberOfpicks){
+    var r = [];
+    for (var i = 0; i < base.length; i++) {
+      base.slice(2)
+      r.concat(base)
+      allPosibleCombinations();
+      console.log(r,base.length);
+    }
+
+  }
+  function cartesian() {
+    var r = [],
+        arg = arguments, max = arg.length-1;
+    function helper(arr, i) {
+        for (var j=0, l=arg[i].length; j<l; j++) {
+            var a = arr.slice(0); // clone arr
+            a.push(arg[i][j]);
+            if (i==max)
+                r.push(a);
+            else
+                helper(a, i+1);
+        }
+    }
+    helper([], 0);
+    return r;
+}
 
   grid.helpers({
     // cardsInDeck: [
@@ -70,19 +159,19 @@ if (Meteor.isClient) {
       return ( i === 12);
     },
     checkForMatch: function(){
-      
+
     },
 
   });
 
 
-  
+
   //  // db.cardsInDeck.insert({color : "green",shape : "x",fill  : "solid", createdAt: new Date() });
   cardsSelected = 0;
 
   grid.onRendered(function(){
   });
-  
+
   card.helpers({
   });
 
@@ -109,16 +198,16 @@ if (Meteor.isClient) {
 
         }});
          // Template.sets_grid.helpers.checkForMatch();
-        
+
         card[cardsSelected] = this._id;
         console.log("clicked on the card: ", cardsSelected  );
         // console.log(Game.find({}, {sort: {createdAt: -1}}).count());
         if (cardsSelected === 2) checkForPatern(cardsSelected);
         cardsSelected++;
-         
+
         }
       }
-      
+
     }
   });
   checkForPatern = function  (numberOfCards) {
@@ -153,7 +242,7 @@ if (Meteor.isClient) {
       //   }})
       //   );
 
-      
+
       for (var i = numberOfCards; i >= 0; i--) {
         Game.update(card[i], {$set: {
           color: colors[randomPropertyAssigment()],
@@ -178,7 +267,7 @@ if (Meteor.isClient) {
       cardsSelected = -1;
       console.log("GOT NOTHING RESETTING CARDS SELECTED ", cardsSelected);
     }
-          
+
   }
 
 
@@ -192,7 +281,7 @@ if (Meteor.isClient) {
       selected: false
     }});
     console.log("Card onRendered: ");
-  });  
+  });
 }
 
 

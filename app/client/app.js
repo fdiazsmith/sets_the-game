4e4e602ca378c0c4934c58c360808d3d33710ed6
Fernando Diaz Smith <fdiazsmith@gmail.com>
2016-03-13 16:35:35 -0400

    configuration settings

--- app/client/app.js
+++ app/client/app.js
@@ -9,6 +9,7 @@
 //this if statement might be superflous
 if (Meteor.isClient) {
   console.log("Client is loaded");
-  var setes = new Setes();
-
+  // var setes = new Setes();
+  //Example of how to access the meteor settings
+	console.log(Meteor.settings);
 };
---
+++ app/server/app.js
@@ -0,0 +1,19 @@
+/**
+  *
+  * S E R V E R
+  *
+  *
+  */
+  
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+  //process.env accesses the variable in the env in shell
+  var Example_shell_var = process.env.CUSTOM_VAR || "undefined probably dev";
+  console.log(Example_shell_var);
+
+  //Meteor settings
+  console.log(Meteor.settings);
+}
+
